99d6c6a563fdcb4bc04135e254c7840bee533bb3
lesang458 <43160752+lesang458@users.noreply.github.com>
2020-06-21 22:20:15 +0700

    Log out

--- app/src/main/java/com/example/dutcomputerlabs_app/LoginActivity.java
+++ app/src/main/java/com/example/dutcomputerlabs_app/LoginActivity.java
@@ -2,10 +2,13 @@ package com.example.dutcomputerlabs_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.content.Intent;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -113,5 +116,14 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         return;
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (getCurrentFocus() != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        }
+        return super.dispatchTouchEvent(ev);
     }
 }
